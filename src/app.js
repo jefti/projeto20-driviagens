a03b3cb51b0481b5e945e192f30d841a864a3c5d
jefti <jeftimeira@gmail.com>
2023-09-08 15:43:35 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import "express-async-errors";
 import cors from "cors";
 import indexRouter from "./routers/index.router.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -7,5 +7,6 @@ const {Pool} = pg;
 const configDatabase = {
     connectionString: process.env.DATABASE_URL,
 };
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
